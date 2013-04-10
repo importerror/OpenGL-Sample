4dec1679aa0eab232aad59778216511d030f8d3c
Santosh Kumar <san00719@yahoo.com>
2013-04-10 10:43:39 +0530

    Animation Effect improved

--- code.cpp
+++ code.cpp
@@ -1,56 +1,51 @@
 #include<glut.h>
 
 int angle=0,x,y;
+int angle1=0;
 
 void keyboard(unsigned char key,int x,int y)
 {
-	if(key=='c')
-	{
-		glClearColor(1,0,0,0);
+if(key=='c')
+{
+glClearColor(1,0,0,0);
 
-	}
+}
 }
 
 void cube(int value)
 {
-	glRotatef(angle,1,0,0);
-	glRotatef(angle,0,1,0);
-	glRotatef(angle,0,0,1);
-	//glTranslatef(2,1,0);
-	glColor3f(0,0,1);
-	glutWireSphere(2,10,10);
-	angle+=0.5;
-	glutPostRedisplay();
-	glFlush();
+glRotatef(angle,1,0,0);
+glRotatef(angle,0,1,0);
+glRotatef(angle,0,0,1);
+glScalef(1,1,1.0001);
+glColor3f(1,0,0);
+glutWireSphere(2,10,10);
+angle+=0.5;
+glutPostRedisplay();
+glFlush();
 }
 
-void color(int value)
-{
-	int z;
-	x=0;
-	glClearColor(x,0,0,0);
-	x+=0.1;
-	if(x==1)
-		x=0;
-	glutPostRedisplay();
-}
 
 
 void drawstring(char *s)
 {
-	int i;
-	glColor3f(0,1,0);
-	glRasterPos2f(0,0);
-	for(i=0;s[i]!='\0';i++)
-		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,s[i]);
+int i;
+glColor3f(0,1,0);
+glRotatef(angle1,1,0,0);
+glRasterPos2f(0,-3);
+for(i=0;s[i]!='\0';i++)
+glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,s[i]);
+angle1+=0.5;
+glutPostRedisplay();
 }
 
 void display()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	drawstring("Santosh");
-	glutTimerFunc(25,cube,0);
-	glFlush();
+glClear(GL_COLOR_BUFFER_BIT);
+glRotatef(angle1,1,0,0);
+drawstring("Santosh");
+glutTimerFunc(25,cube,0);
+glFlush();
 }
 
 
@@ -62,17 +57,17 @@ void myReshape(int w, int h)
   gluPerspective(65.0, (GLfloat) w / (GLfloat) h, 1.0, 20.0);
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
-  glTranslatef(0.0, 1.2, -5.5); 
+  glTranslatef(0.0, 1.2, -5.5);
 }
 
 void main()
 {
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-	glutInitWindowSize(800,600);
-	glutCreateWindow("Splash Screen");
-	glutDisplayFunc(display);
-	glutReshapeFunc(myReshape);
-	glutTimerFunc(25,color,0);
-	glutKeyboardFunc(keyboard);
-	glutMainLoop();
+glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+glutInitWindowSize(800,600);
+glutCreateWindow("Splash Screen");
+glutDisplayFunc(display);
+glutReshapeFunc(myReshape);
+glutTimerFunc(25,cube,0);
+glutKeyboardFunc(keyboard);
+glutMainLoop();
 }
